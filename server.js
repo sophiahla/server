00b228aa93d1b9e15405ceb8a1729da46fa15926
sophiahla <38471921+sophiahla@users.noreply.github.com>
2019-05-07 20:32:53 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+ 
+http.createServer(function(request, response){
+     
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+     
+    response.end("hello");
+}).listen(3000);
